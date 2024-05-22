6dc63c59aacd91a35f0b6ab90befe7c6a172c793
allanmeireles <henriquemeireles129@gmail.com>
2024-05-22 19:37:55 -0300

    criação da classe client

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,15 @@
 package application;
-
 import java.sql.Connection;
-
 import db.DB;
+import entities.client;
+
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection comn = DB.getConnection();
-		DB.closeConnection();
+		client obj = new client(1,"allan","rua azul","40028922");
+		System.out.println(obj);
 		
 	}
 }
---
+++ src/entities/client.java
@@ -0,0 +1,80 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class client implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String name; 
+	private String address; 
+	private String telephone; 
+	
+	public client(){
+		
+	}
+
+	public client(int id, String name, String address, String telephone) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.telephone = telephone;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		client other = (client) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "client [id=" + id + ", name=" + name + ", address=" + address + ", telephone=" + telephone + "]";
+	}
+	
+	
+	
+	
+}
